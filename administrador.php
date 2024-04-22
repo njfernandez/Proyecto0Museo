4949944d427c04bed56f2b0f9b07bd1531b1b4e0
Nestor <nestorjfernandez5@gmail.com>
2024-04-22 17:02:24 -0300

    acomode las cosas

--- administrador.php
+++ administrador.php
@@ -1,13 +1,11 @@
-<?php
-
+<?php 
 require_once 'BD/conexion.php';
-$sql="SELECT * FROM `pieza`,donante WHERE Donante_idDonante = donante.idDonante";
+$sql="SELECT * FROM usuario";
 $result=mysqli_query($conex,$sql);
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-    <head>
+<head>
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
@@ -49,7 +47,7 @@ $result=mysqli_query($conex,$sql);
             <div id="layoutSidenav_content">
                 <main>
                     <div class="container-fluid px-4">
-                        <h1 class="mt-4">Tabla de piezas</h1>
+                        <h1 class="mt-4">Tabla de Usuarios y Donantes</h1>
         
                        
                         <div class="card mb-4">
@@ -92,34 +90,29 @@ $result=mysqli_query($conex,$sql);
                     <a href="#" class="datatable">ID</a>
                 </th>
                 <th data-sortable="true" aria-sort="ascending" class="datatable" style="width: 19.30835734870317%;">
-                    <a href="#" class="datatable">N° de Articulo</a>
+                    <a href="#" class="datatable">DNI</a>
                 </th>
                 <th data-sortable="true" aria-sort="ascending" class="datatable" style="width: 19.30835734870317%;">
-                    <a href="#" class="datatable">Especie</a>
+                    <a href="#" class="datatable">nombre</a>
                 </th>
                 <th data-sortable="true" aria-sort="ascending" class="datatable" style="width: 19.30835734870317%;">
-                    <a href="#" class="datatable">Estado</a>
+                    <a href="#" class="datatable">apellido</a>
                 </th>
                 <th data-sortable="true" aria-sort="ascending" class="datatable" style="width: 19.30835734870317%;">
-                    <a href="#" class="datatable">Fecha</a>
+                    <a href="#" class="datatable">email</a>
                 </th>
                 <th data-sortable="true" aria-sort="ascending" class="datatable" style="width: 19.30835734870317%;">
-                    <a href="#" class="datatable">Cantidad</a>
+                    <a href="#" class="datatable">clave</a>
                 </th>
                 <th data-sortable="true" aria-sort="ascending" class="datatable" style="width: 19.30835734870317%;">
-                    <a href="#" class="datatable">clasificacion</a>
-                </th>
-                <th data-sortable="true" aria-sort="ascending" class="datatable" style="width: 19.30835734870317%;">
-                    <a href="#" class="datatable">Observacion</a>
-                </th>
-                <th data-sortable="true" aria-sort="ascending" class="datatable" style="width: 19.30835734870317%;">
-                    <a href="#" class="datatable">Donante</a>
+                    <a href="#" class="datatable">fecha_alta</a>
                 </th>
                 <th data-sortable="true" aria-sort="ascending" class="datatable" style="width: 19.30835734870317%;">
                     <a href="#" class="datatable">Accion</a>
                 </th>
-               
             
+            
+               
             </thead>
         
             <?php
@@ -136,34 +129,28 @@ $result=mysqli_query($conex,$sql);
         
                <tr>
                     
-               <th scope="row"><?php echo $fila["idPieza"]; ?></th>
-               <td><?php echo $fila["num_inventario"]; ?></td>
-               <td><?php echo $fila["especie"]; ?></td>
-               <td><?php echo $fila["estado_conservacion"]; ?></td>
-               <td><?php echo $fila["fecha_ingreso"]; ?></td>
-               <td><?php echo $fila["cantidad de piezas"]; ?></td>
-               <td><?php echo $fila["clasificacion"]; ?></td>
-               <td><?php echo $fila["observacion"]; ?></td>
-               <td><?php echo $fila["Donante_idDonante"]; ?></td>
-               <!--
-                <td><?php //echo $fila["descripcion"]; ?></td>
-               <td><?php //echo $fila["observacion"]; ?></td>
-               -->
+               <th scope="row"><?php echo $fila["idUsuario"]; ?></th>
+               <td><?php echo $fila["dni"]; ?></td>
+               <td><?php echo $fila["nombre"]; ?></td>
+               <td><?php echo $fila["apellido"]; ?></td>
+               <td><?php echo $fila["email"]; ?></td>
+               <td><?php echo $fila["clave"]; ?></td>
+               <td><?php echo $fila["fecha_alta"]; ?></td>
                <td>
                <div class="d-sm-inline-block">
                 <form action="formEditarPieza.php" method="post">
-		          <input type="hidden" name="id" value="<?php echo $fila["idpiezas"]; ?>">
+		          <input type="hidden" name="id" value="<?php echo $fila["idUsuario"]; ?>">
 		          <button class="btn border btn-outline-success btn-sm" type="submit" name="btneditar" id="btneditar">Editar</button>
                 </form>
                 </div>
                <div class="d-sm-inline-block"><form action="formEliminarPieza.php" method="post">
-		          <input type="hidden" name="id" value="<?php echo $fila["idpiezas"]; ?>">
+		          <input type="hidden" name="id" value="<?php echo $fila["idUsuario"]; ?>">
 		          <button class="btn border btn-outline-danger btn-sm" type="submit" name="btnborrar" id="btnborrar">Borrar</button>
                 </form>
             </div>
             <div class="d-sm-inline-block">
                 <form action="detalles.php" method="post">
-		          <input type="hidden" name="id" value="<?php echo $fila["idpiezas"]; ?>">
+		          <input type="hidden" name="id" value="<?php echo $fila["idUsuario"]; ?>">
 		          <button class="btn border btn-outline-info btn-sm" type="submit" name="btndetalles" id="btndetalles">Detalles</button>
                 </form>
                 </div>
@@ -185,16 +172,16 @@ $result=mysqli_query($conex,$sql);
 	     }else{
 
           echo "</table></div>";
-          echo "<div class='container text-center lead my-3 py-3'><div class='alert alert-danger my-5 py-4'><p><em>No existen piezas! </em><a href='index.php' class='text-primary lead ms-2'>Volver</a></p></div></div>";
+          echo "<div class='container text-center lead my-3 py-3'><div class='alert alert-danger my-5 py-4'><p><em>No existen usuarios! </em><a href='index.php' class='text-primary lead ms-2'>Volver</a></p></div></div>";
          }
 	   ?>  
  
 
-                            </div>
-                        </div>
                     </div>
-                </main>
-                <footer class="py-4 bg-light mt-auto">
+                </div>
+            </div>
+    </main>
+        <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid px-4">
                         <div class="d-flex align-items-center justify-content-between small">
                             <div class="text-muted">Copyright &copy; Your Website 2023</div>
@@ -212,5 +199,7 @@ $result=mysqli_query($conex,$sql);
         <script src="js/scripts.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
         <script src="js/datatables-simple-demo.js"></script>
-    </body>
+
+
+
 </html>
